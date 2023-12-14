128ab3d7fd534fcde3b6bb4a4c69360f0090c4d1
Enablex <pankaj.p@vcloudx.com>
2023-12-14 14:48:50 +0530

    sdk update

--- app/src/main/java/com/enablex/demoenablex/web_communication/WebConstants.kt
+++ app/src/main/java/com/enablex/demoenablex/web_communication/WebConstants.kt
@@ -11,8 +11,8 @@ object WebConstants {
     /*The following information required, Only when kTry = true, When you hosted your own webservice remove these fileds*/
 
     /*Use enablec portal to create your app and get these following credentials*/
-    val kAppId = "App-ID"
-    val kAppkey = "App-Key"
+    val kAppId = "App--ID"
+    val kAppkey = "App--Key"
 
     val getRoomId = "createRoom/"
     val getRoomIdCode = 1
